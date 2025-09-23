9eee9ad5c677f94ac07bc9afb849b2c4904cbbe1
spencercjh <spencercjh@gmail.com>
2025-09-23 18:40:50 +0800

    feat: add placeholder for Windows experiment models
    Signed-off-by: spencercjh <spencercjh@gmail.com>

---
+++ exec/model/model_windows.go
@@ -0,0 +1,10 @@
+package model
+
+import "github.com/chaosblade-io/chaosblade-spec-go/spec"
+
+// GetAllExpModels returns the experiment model specs in the project.
+// Support for other project about chaosblade
+func GetAllExpModels() []spec.ExpModelCommandSpec {
+	// TODO: implement Windows experiment models
+	return []spec.ExpModelCommandSpec{}
+}

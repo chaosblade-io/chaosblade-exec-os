07a39781e7c80ca9abab73c629cbe3ce65909b46
spencercjh <spencercjh@gmail.com>
2025-10-15 15:00:16 +0800

    fix: correct typos in comments regarding CPU quota support for non-linux systems
    Signed-off-by: spencercjh <spencercjh@gmail.com>

--- pkg/automaxprocs/runtime/cpu_quota_unsupported.go
+++ pkg/automaxprocs/runtime/cpu_quota_unsupported.go
@@ -6,7 +6,7 @@ import (
 	"context"
 )
 
-// GetCPUQuotaToCPUCntByPidForCgroups1 is not support for non-linux systems because cgroups is a Linux kernal feature.
+// GetCPUQuotaToCPUCntByPidForCgroups1 is unsupported for non-linux systems because cgroups is a Linux kernel feature.
 func GetCPUQuotaToCPUCntByPidForCgroups1(
 	_ context.Context,
 	_, _ string,
@@ -16,7 +16,7 @@ func GetCPUQuotaToCPUCntByPidForCgroups1(
 	return -1, 1.0, CPUQuotaUndefined, nil
 }
 
-// GetCPUQuotaToCPUCntByPidForCgroups2 is not support for non-linux systems because cgroups is a Linux kernal feature.
+// GetCPUQuotaToCPUCntByPidForCgroups2 is unsupported for non-linux systems because cgroups is a Linux kernel feature.
 func GetCPUQuotaToCPUCntByPidForCgroups2(
 	_ context.Context,
 	_, _ string,
